244c876713d373b94e4301df0ae52cce288c8de6
Marco Ballario <marco.ballario1997@gmail.com>
2022-07-05 14:44:18 +0200

    Update produce-data script to remove intermediate data folder

--- produce-data.sh
+++ produce-data.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-
-declare -a repos=("FastCSV" "java-jwt" "jsoup" "Java-WebSocket")
+declare -a repos=(
+    "FastCSV"
+    "java-jwt"
+    "jsoup"
+    "Java-WebSocket"
+)
 declare -a urls=(
     "https://github.com/osiegmar/FastCSV.git"
     "https://github.com/auth0/java-jwt.git"
@@ -14,62 +18,55 @@ declare -a versions=(
     "jsoup-1.15.2 jsoup-1.15.1"
     "v1.5.3 v1.5.2"
 )
-Color_Off='\033[0m'
-Yellow='\033[0;33m'
-Green='\033[0;32m'
-Blue='\033[0;34m'
+color_off='\033[0m'
+yellow='\033[0;33m'
+green='\033[0;32m'
+blue='\033[0;34m'
 
 # Download and build rust-code-analysis
-if [ -d "./rust-code-analysis" ]
+if [ -d "./repositories/rust-code-analysis" ]
 then
-    echo -e "${Yellow}Updating rust-code-analysis...${Color_Off}"
-    cd rust-code-analysis
+    echo -e "${yellow}Updating rust-code-analysis...${color_off}"
+    cd repositories/rust-code-analysis/
     git checkout -f master
     git pull
     cargo build -p rust-code-analysis-cli --release
-    cd ../
-    echo -e "${Green}rust-Code-analysis updated!${Color_Off}"
+    cd ../../
+    echo -e "${green}rust-code-analysis updated!${color_off}"
 else
-    echo -e "${Yellow}Cloning and building rust-code-analysis...${Color_Off}"
+    echo -e "${yellow}Cloning and building rust-code-analysis...${color_off}"
+    cd repositories/
     git clone "https://github.com/mozilla/rust-code-analysis.git"
-    cd rust-code-analysis
+    cd rust-code-analysis/
     cargo build -p rust-code-analysis-cli --release
-    cd ../
-    echo -e "${Green}rust-code-analysis ready!${Color_Off}"
+    cd ../../
+    echo -e "${green}rust-code-analysis ready!${color_off}"
 fi
-read -p "$(echo -e ${Blue}"Press enter to continue"${Color_Off})"
-
+read -p "$(echo -e ${blue}"Press enter to continue"${color_off})"
 
 # Download repositories to measure
-if [ -d "./repositories" ]
-then
-    echo -e "${Yellow}Repositories already downloaded${Color_Off}"
-    cd repositories
-    for i in ${!urls[@]}
-    do
-        echo -e "${Yellow}Updating ${repos[$i]}...${Color_Off}"
-        cd ./${repos[$i]}
+for i in ${!urls[@]}
+do
+    declare -a repo=${repos[$i]}
+    declare -a url=${urls[$i]}
+    if [ -d "./repositories/$repo" ]
+    then
+        echo -e "${yellow}Updating $repo...${color_off}"
+        cd ./repositories/$repo
         git checkout -f master
         git pull
-        cd ../
-        echo -e "${Green}${repos[$i]} updated!${Color_Off}"
-    done
-    cd ../
-else
-    echo -e "${Yellow}Download repositories...${Color_Off}"
-    mkdir repositories
-    cd repositories
-    for i in ${!urls[@]}
-    do
-        echo -e "${Yellow}Cloning ${repos[$i]}...${Color_Off}"
-        git clone ${urls[$i]}
-        echo -e "${Green}${repos[$i]} cloned!${Color_Off}"
-    done
-    cd ../
-    echo -e "${Green}Repositories downloaded!${Color_Off}"
-fi
-read -p "$(echo -e ${Blue}"Press enter to continue"${Color_Off})"
-
+        cd ../../
+        echo -e "${green}$repo updated!${color_off}"
+    else
+        echo -e "${yellow}Cloning $repo...${color_off}"
+        mkdir repositories/$repo
+        cd repositories/$repo
+        git clone $url
+        cd ../../
+        echo -e "${green}$repo cloned!${color_off}"
+    fi
+done
+read -p "$(echo -e ${blue}"Press enter to continue"${color_off})"
 
 # Produce metrics
 for i in "${!repos[@]}"
@@ -77,13 +74,13 @@ do
     declare -a repo="${repos[$i]}"
     for version in ${versions[$i]}
     do
-        echo -e "${Yellow}Producing $repo $version metrics...${Color_Off}"
+        echo -e "${yellow}Producing $repo $version metrics...${color_off}"
         mkdir -p ./data/$repo/$version
         cd ./repositories/$repo/
         git checkout $version
-        cd ../../rust-code-analysis/
-        cargo run -p rust-code-analysis-cli --release -- -m -p ../repositories/$repo/ -O json -o ../data/$repo/$version/ -l Java
-        cd ../
-        echo -e "${Green}$repo $version metrics produced!${Color_Off}"
+        cd ../rust-code-analysis/
+        cargo run -p rust-code-analysis-cli --release -- -m -p ../$repo/ -O json -o ../../data/$repo/$version/ -l Java
+        cd ../../
+        echo -e "${green}$repo $version metrics produced!${color_off}"
     done
 done
