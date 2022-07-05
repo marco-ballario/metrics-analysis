011e261502d5f8b6a25c4428602d1569d535e512
Marco Ballario <marco.ballario1997@gmail.com>
2022-07-05 17:36:40 +0200

    Add NPM and NPA static analysis graphs

--- static-analysis.py
+++ static-analysis.py
@@ -1,58 +1,60 @@
+import os
 import json
 import glob
-import os
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
-import numpy as np
 
-
-def read_measures(repository_name, sum_list, avg_list, min_list, max_list):
-    magnitude = []
-    for filename in glob.iglob("data/" + repository_name + '**/**/*.json', recursive=True):
+def read_measures(repository_name, version, abc, npm, npa):
+    magn = []
+    pub_met = []
+    pub_att = []
+    for filename in glob.iglob("data/" + repository_name + "/" + version + '/**/*.json', recursive=True):
         with open(filename, "r") as file:
             jsonData = json.load(file)
-            magnitude.append((float(jsonData["metrics"]["abc"]["magnitude"])))
+            magn.append((float(jsonData["metrics"]["abc"]["magnitude"])))
+            pub_met.append((float(jsonData["metrics"]["npm"]["total"])))
+            pub_att.append((float(jsonData["metrics"]["npa"]["total"])))
+
             print(os.path.basename(jsonData["name"]))
             print(jsonData["metrics"]["abc"]["magnitude"])
-    magnitude = [float(x) for x in magnitude]
-    print(magnitude)
-    min_list.append(min(magnitude))
-    max_list.append(max(magnitude))
-    avg_list.append(sum(magnitude) / len(magnitude))
-    sum_list.append(sum(magnitude))
-    print(min_list)
-    print(max_list)
-    print(avg_list)
+    magn = [float(x) for x in magn]
+    pub_met = [float(x) for x in pub_met]
+    print(magn)
+    abc.append( [min(magn), max(magn), sum(magn)/len(magn), sum(magn)] )
+    npm.append( [min(pub_met), max(pub_met), sum(pub_met)/len(pub_met), sum(pub_met)] )
+    npa.append( [min(pub_att), max(pub_att), sum(pub_att)/len(pub_att), sum(pub_att)] )
+    print(abc)
     return
 
 
-def plot_measures(repositories, sum_list, avg_list, min_list, max_list):
+def plot_measures(repositories, versions, abc, title, y_name, file):
     figure(figsize=(16, 10), dpi=80)
     plt.subplots_adjust(top=0.92, bottom=0.28)
     plt.rcParams.update({'font.size': 14})
     plt.xticks(rotation='vertical')
     plt.xlabel("Repositories")
-    plt.ylabel("Magnitude")
-    plt.title("ABC")
+    plt.ylabel(y_name)
+    plt.title(title)
     for i in range(0, len(repositories)):
         x = np.array([repositories[i] + "_sum", repositories[i] + "_min", repositories[i] + "_max", repositories[i] + "_avg"])
-        y = np.array([sum_list[i], min_list[i], max_list[i], avg_list[i]])
-        plt.bar(x, y, 0.5, label=repositories[i])
+        y = np.array([abc[i][3], abc[i][0], abc[i][1], abc[i][2]])
+        plt.bar(x, y, 0.5, label=repositories[i] + " " + versions[i])
     plt.legend()
-    plt.savefig("./graphs/static-analysis/abc-static-analysis.png")
+    plt.savefig("./graphs/static-analysis/" + file)
     return
-
 
 def static_analysis():
-    repositories = ["Java-WebSocket", "jsoup", "java-jwt", "FastCSV"]
-    sum_list = []
-    avg_list = []
-    min_list = []
-    max_list = []
-    for repository in repositories:
-        read_measures(repository, sum_list, avg_list, min_list, max_list)
-    plot_measures(repositories, sum_list, avg_list, min_list, max_list) 
+    repos = ["FastCSV", "java-jwt", "jsoup", "Java-WebSocket"]
+    versions = ["v2.2.0", "4.0.0", "jsoup-1.15.2", "v1.5.3"]
+    abc = []
+    npm = []
+    npa = []
+    for i in range(len(repos)):
+        read_measures(repos[i], versions[i], abc, npm, npa)
+    plot_measures(repos, versions, abc, "ABC", "Magnitude", "abc-static-analysis.png")
+    plot_measures(repos, versions, npm, "NPM", "Number of public methods", "npm-static-analysis.png")
+    plot_measures(repos, versions, npa, "NPA", "Number of public attributes", "npa-static-analysis.png")
     return
-
 
 static_analysis()
