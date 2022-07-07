bd435662ac330bb33852db0a7757bd593d2947dc
Marco Ballario <marco.ballario1997@gmail.com>
2022-07-07 16:55:48 +0200

    Add size metrics comparison graph

--- static-analysis.py
+++ static-analysis.py
@@ -5,11 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-def read_measures(repository_name, version, abc, npm, npa, wmc):
+def read_measures(repository_name, version, abc, npm, npa, wmc, size):
     magn = []
     pub_met = []
     pub_att = []
     wmc_val = []
+    hal_val = []
+    loc_val = []
+    cyc_val = []
     for filename in glob.iglob("data/" + repository_name + "/" + version + '/**/*.json', recursive=True):
         with open(filename, "r") as file:
             jsonData = json.load(file)
@@ -17,6 +20,9 @@ def read_measures(repository_name, version, abc, npm, npa, wmc):
             pub_met.append((float(jsonData["metrics"]["npm"]["total"])))
             pub_att.append((float(jsonData["metrics"]["npa"]["total"])))
             wmc_val.append((float(jsonData["metrics"]["wmc"]["wmc"])))
+            hal_val.append((float(jsonData["metrics"]["halstead"]["estimated_program_length"])))
+            loc_val.append((float(jsonData["metrics"]["loc"]["ploc"])))
+            cyc_val.append((float(jsonData["metrics"]["cyclomatic"]["sum"])))
 
             print(os.path.basename(jsonData["name"]))
             print(jsonData["metrics"]["abc"]["magnitude"])
@@ -29,6 +35,7 @@ def read_measures(repository_name, version, abc, npm, npa, wmc):
     npm.append( [min(pub_met), max(pub_met), sum(pub_met)/len(pub_met), sum(pub_met)] )
     npa.append( [min(pub_att), max(pub_att), sum(pub_att)/len(pub_att), sum(pub_att)] )
     wmc.append( [min(wmc_val), max(wmc_val), sum(wmc_val)/len(wmc_val), sum(wmc_val)] )
+    size.append( [sum(magn), sum(hal_val), sum(loc_val), sum(cyc_val)] )
     print(abc)
     return
 
@@ -49,6 +56,22 @@ def plot_measures(repositories, versions, abc, title, y_name, file):
     plt.savefig("./graphs/static-analysis/" + file)
     return
 
+def plot_size_measures(repositories, versions, val, title, y_name, file):
+    figure(figsize=(16, 10), dpi=80)
+    plt.subplots_adjust(top=0.92, bottom=0.28)
+    plt.rcParams.update({'font.size': 14})
+    plt.xticks(rotation='vertical')
+    plt.xlabel("Repositories")
+    plt.ylabel(y_name)
+    plt.title(title)
+    for i in range(0, len(repositories)):
+        x = np.array([repositories[i] + "_abc", repositories[i] + "_hal", repositories[i] + "_ploc", repositories[i] + "_cyc"])
+        y = np.array([val[i][0], val[i][1], val[i][2], val[i][3]])
+        plt.bar(x, y, 0.5, label=repositories[i] + " " + versions[i])
+    plt.legend()
+    plt.savefig("./graphs/static-analysis/" + file)
+    return
+
 def static_analysis():
     repos = ["FastCSV", "java-jwt", "jsoup", "Java-WebSocket"]
     versions = ["v2.2.0", "4.0.0", "jsoup-1.15.2", "v1.5.3"]
@@ -56,12 +79,14 @@ def static_analysis():
     npm = []
     npa = []
     wmc = []
+    size = []
     for i in range(len(repos)):
-        read_measures(repos[i], versions[i], abc, npm, npa, wmc)
+        read_measures(repos[i], versions[i], abc, npm, npa, wmc, size)
     plot_measures(repos, versions, abc, "ABC", "Magnitude", "abc-static-analysis.png")
     plot_measures(repos, versions, npm, "NPM", "Number of public methods", "npm-static-analysis.png")
     plot_measures(repos, versions, npa, "NPA", "Number of public attributes", "npa-static-analysis.png")
     plot_measures(repos, versions, wmc, "WMC", "Weighted methods per class", "wmc-static-analysis.png")
+    plot_size_measures(repos, versions, size, "Size measures", "Values", "size-metrics-comparison.png")
     return
 
 static_analysis()
