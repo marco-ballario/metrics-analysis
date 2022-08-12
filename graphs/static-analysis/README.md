876a2401e59dd9d5a2e8e78628707f4f01a03623
Marco Ballario <marco.ballario1997@gmail.com>
2022-08-12 02:42:39 +0200

    Improve static analysis graphs

--- graphs/static-analysis/README.md
+++ graphs/static-analysis/README.md
@@ -1,43 +1,73 @@
-# static-analysis
+# Static Analysis
+
+## Repositories
+| Name | Version | Files | Source |
+| --- | --- | --- | --- |
+| [Java-WebSocket](https://github.com/TooTallNate/Java-WebSocket) | v1.5.3 | 175 | [Maven Central](https://mvnrepository.com/artifact/org.java-websocket/Java-WebSocket) |
+| [jsoup](https://github.com/jhy/jsoup) | jsoup-1.15.2 | 136 | [Maven Central](https://mvnrepository.com/artifact/org.jsoup/jsoup) |
+| [java-jwt](https://github.com/auth0/java-jwt) | 4.0.0 | 75 | [Maven Central](https://mvnrepository.com/artifact/com.auth0/java-jwt) |
+| [FastCSV](https://github.com/osiegmar/FastCSV) | v2.2.0 | 39 | [Maven Central](https://mvnrepository.com/artifact/de.siegmar/fastcsv) |
 
 ## Graphs
-- [Weighted Methods per Class (WMC)](#weighted-methods-per-class-wmc)
-- [Magnitude (ABC)](#magnitude-abc)
-- [Number of Public Methods (NPM)](#number-of-public-methods-npm)
-- [Class Operation Accessibility (COA)](#class-operation-accessibility-coa)
-- [Number of Public Attributes (NPA)](#number-of-public-attributes-npa)
-- [Class Data Accessibility (CDA)](#class-data-accessibility-cda)
-- [Size metrics comparison](#size-metrics-comparison)
-- [Complexity metrics comparison](#complexity-metrics-comparison)
+- [Cumulative measures](#cumulative-measures)
+    - [Magnitude (ABC)](#magnitude-abc)
+    - [Weighted Methods per Class (WMC)](#weighted-methods-per-class-wmc)
+    - [Number of Public Methods (NPM)](#number-of-public-methods-npm)
+    - [Number of Public Attributes (NPA)](#number-of-public-attributes-npa)
+- [Metric comparisons](#metric-comparisons)
+    - [Size metrics](#size-metrics)
+    - [Complexity metrics](#complexity-metrics)
+- [Visibility measures](#visibility-measures)
+    - [Methods](#methods)
+    - [Percentage of methods](#percentages-of-methods)
+    - [Attributes](#attributes)
+    - [Percentage of attributes](#percentage-of-attributes)
 
-### Weighted Methods per Class (WMC)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./wmc-static-analysis.png" width="100%"></div>
+### Cumulative measures
+This kind of graph represents the sum, maximum and average cumulative measures for each repository.
 
-### Magnitude (ABC)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./abc-static-analysis.png" width="100%"></div>
+#### Magnitude (ABC)
+The objective of the graph is to show that the magnitude can be used to measure repositories size. The magnitude is often directly proportional to the number of source files in the repository.
+<div align="center"><img src="./cumulative-measures-abc.png" width="100%"></div>
 
-### Number of Public Methods (NPM)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./npm-static-analysis.png" width="100%"></div>
+#### Weighted Methods per Class (WMC)
+The objective of the graph is to show that the WMC can be used to measure repositories complexity.
+<div align="center"><img src="./cumulative-measures-wmc.png" width="100%"></div>
 
-### Class Operation Accessibility (COA)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./coa-static-analysis.png" width="100%"></div>
+#### Number of Public Methods (NPM)
+The objective of the graph is to show that the NPM can be used to measure repositories security.
+<div align="center"><img src="./cumulative-measures-npm.png" width="100%"></div>
 
-### Number of Public Attributes (NPA)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./npa-static-analysis.png" width="100%"></div>
+#### Number of Public Attributes (NPA)
+The objective of the graph is to show that the NPA can be used to measure repositories security.
+<div align="center"><img src="./cumulative-measures-npa.png" width="100%"></div>
 
-### Class Data Accessibility (CDA)
-Sum, minimum, maximum and average over a set of repository files.
-<div align="center"><img src="./cda-static-analysis.png" width="100%"></div>
+### Metric comparisons
+This kind of graph represent more than one metric for each repository.
 
-### Size metrics comparison
-Compare ABC magnitude, Halstead estimated program length, PLOC and Cyclomatic Complexity metrics for a set of repositories.
-<div align="center"><img src="./size-metrics-comparison.png" width="100%"></div>
+#### Size metrics
+The objective of the graph is to show that ABC magnitude, Halstead Estimated Program Length, PLOC and Cyclomatic Complexity metrics for a set of repositories provide similar repository size information.
+<div align="center"><img src="./metric-comparisons-size.png" width="100%"></div>
 
-### Complexity metrics comparison
-Compare WMC and Cyclomatic Complexity metrics for a set of repositories.
-<div align="center"><img src="./complexity-metrics-comparison.png" width="100%"></div>
+#### Complexity metrics
+The objective of the graph is to show that WMC and Cyclomatic Complexity metrics for a set of repositories provide similar repository complexity information.
+<div align="center"><img src="./metric-comparisons-complexity.png" width="100%"></div>
+
+### Visibility measures
+This kind of graph represent public and non-public members for each repository.
+
+#### Methods
+The objective of the graph is to show how many public and non-public methods have been declared in repositories.
+<div align="center"><img src="./visibility-measures-npm.png" width="100%"></div>
+
+#### Percentages of methods
+The objective of the graph is to show the percentages of public and non-public methods declared in repositories.
+<div align="center"><img src="./visibility-percentages-npm.png" width="100%"></div>
+
+#### Attributes
+The objective of the graph is to show how many public and non-public attributes have been declared in repositories.
+<div align="center"><img src="./visibility-measures-npa.png" width="100%"></div>
+
+#### Percentage of attributes
+The objective of the graph is to show the percentages of public and non-public attributes declared in repositories.
+<div align="center"><img src="./visibility-percentages-npa.png" width="100%"></div>
--- static-analysis.py
+++ static-analysis.py
@@ -1,195 +1,183 @@
-import os
 import json
 import glob
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-def read_measures(repository_name, version, abc, npm, npa, wmc, coa, cda, size, complexity):
-    magn = []
-    pub_met = []
-    pub_att = []
-    wmc_val = []
-    hal_val = []
-    loc_val = []
-    cyc_val = []
-    coa_val = []
-    cda_val = []
-    for filename in glob.iglob("data/" + repository_name + "/" + version + '/**/*.json', recursive=True):
+def read_measures(repository_name, version, abc, npm, npa, wmc, size, compl, met, att):
+    abc_mag = []
+    npm_tot = []
+    npa_tot = []
+    wmc_tot = []
+    hal_epl = []
+    loc_ploc = []
+    cyc_sum = []
+    npm_avg = []
+    npa_avg = []
+    npn_tot_met = []
+    npa_tot_att = []
+
+    for filename in glob.iglob("data/" + repository_name + "/" + version + "/**/*.json", recursive=True):
         with open(filename, "r") as file:
             jsonData = json.load(file)
-            magn.append(float(jsonData["metrics"]["abc"]["magnitude"]))
-            pub_met.append(float(jsonData["metrics"]["npm"]["total"]))
-            pub_att.append(float(jsonData["metrics"]["npa"]["total"]))
-            coa_val.append(float(jsonData["metrics"]["npm"]["average"] or 0))
-            cda_val.append(float(jsonData["metrics"]["npa"]["average"] or 0))
-            wmc_val.append(float(jsonData["metrics"]["wmc"]["wmc"]))
-            hal_val.append(float(jsonData["metrics"]["halstead"]["estimated_program_length"]))
-            loc_val.append(float(jsonData["metrics"]["loc"]["ploc"]))
-            cyc_val.append(float(jsonData["metrics"]["cyclomatic"]["sum"]))
+            abc_mag.append(float(jsonData["metrics"]["abc"]["magnitude"]))
+            wmc_tot.append(float(jsonData["metrics"]["wmc"]["total"]))
+            npm_tot.append(float(jsonData["metrics"]["npm"]["total"]))
+            npa_tot.append(float(jsonData["metrics"]["npa"]["total"]))
+            npm_avg.append(float(jsonData["metrics"]["npm"]["average"] or 0))
+            npa_avg.append(float(jsonData["metrics"]["npa"]["average"] or 0))
+            npn_tot_met.append(float(jsonData["metrics"]["npm"]["total_methods"]))
+            npa_tot_att.append(float(jsonData["metrics"]["npa"]["total_attributes"]))
+            hal_epl.append(float(jsonData["metrics"]["halstead"]["estimated_program_length"]))
+            loc_ploc.append(float(jsonData["metrics"]["loc"]["ploc"]))
+            cyc_sum.append(float(jsonData["metrics"]["cyclomatic"]["sum"]))
 
-            # print(os.path.basename(jsonData["name"]))
-            # print(jsonData["metrics"]["abc"]["magnitude"])
-    magn = [float(x) for x in magn]
-    pub_met = [float(x) for x in pub_met]
-    pub_att = [float(x) for x in pub_att]
-    wmc_val = [float(x) for x in wmc_val]
-    # print(magn)
-    abc.append( [max(magn), sum(magn)/len(magn), sum(magn)] )
-    npm.append( [max(pub_met), sum(pub_met)/len(pub_met), sum(pub_met)] )
-    npa.append( [max(pub_att), sum(pub_att)/len(pub_att), sum(pub_att)] )
-    cda.append( [max(cda_val), sum(cda_val)/len(cda_val), sum(cda_val)] )
-    coa.append( [max(coa_val), sum(coa_val)/len(coa_val), sum(coa_val)] )
-    wmc.append( [max(wmc_val), sum(wmc_val)/len(wmc_val), sum(wmc_val)] )
-    size.append( [sum(magn), sum(hal_val), sum(loc_val), sum(cyc_val)] )
-    print( [sum(magn), sum(hal_val), sum(loc_val), sum(cyc_val)] )
-    complexity.append( [sum(wmc_val), sum(cyc_val)] )
-    # print(abc)
+    abc.append([sum(abc_mag), max(abc_mag), sum(abc_mag)/len(abc_mag)])
+    wmc.append([sum(wmc_tot), max(wmc_tot), sum(wmc_tot)/len(wmc_tot)])
+    npm.append([sum(npm_tot), max(npm_tot), sum(npm_tot)/len(npm_tot)])
+    npa.append([sum(npa_tot), max(npa_tot), sum(npa_tot)/len(npa_tot)])
+    size.append([sum(abc_mag), sum(hal_epl), sum(loc_ploc), sum(cyc_sum)])
+    compl.append([sum(wmc_tot), sum(cyc_sum)])
+    met.append([sum(npm_tot), sum(npn_tot_met)])
+    att.append([sum(npa_tot), sum(npa_tot_att)])
     return
 
-
-def plot_measures(repositories, versions, abc, title, y_name, file):
-    V_axis = []
-    Y_axis=[]
+def plot_cumulative_measures(repositories, values, legend_loc, title, file):
+    x = []
+    y = []
     for i in range(0, len(repositories)):
-        V_axis.append([repositories[i] + "_sum", repositories[i] + "_max", repositories[i] + "_avg"])
-        Y_axis.append([abc[i][2], abc[i][0], abc[i][1]])
-    min_y=float("inf")
-    max_y=0
-    for y in Y_axis:
-        for value in y:
-            min_y=min(min_y,value)
-            max_y=max(max_y,value)
-    plt.rcParams.update({'font.size': 24})
-    figure(figsize=(42,22), dpi=80)
-    for i in range(0,len(repositories)):
-        bar=plt.barh(V_axis[i], Y_axis[i], 0.66, label = repositories[i])
-        plt.bar_label(bar,padding=5, fontweight='bold')
+        x.append([values[i][2], values[i][1], values[i][0]])
+        y.append([repositories[i] + "_avg", repositories[i] + "_max", repositories[i] + "_sum"])
 
-    plt.xticks(get_ticks(Y_axis),rotation=45)
-    plt.xlim(min_y)
-    plt.xlabel("Repositories",loc='left',labelpad = 10, fontweight='bold', fontsize=24)
-    plt.ylabel(y_name,loc='bottom',labelpad = 10, fontweight='bold', fontsize=24)
+    font_size = 24
+    figure(figsize=(42, 22), dpi=80)
+    plt.rcParams.update({"font.size": font_size})
+
+    for i in range(0,len(repositories)):
+        bar=plt.barh(y[i], x[i], 0.66, label=repositories[i])
+        plt.bar_label(bar, padding=5, fontweight="bold")
+
+    plt.xticks(rotation=45)
+    plt.xlabel("Metric Values", loc="left", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.ylabel("Repositories", loc="bottom", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.title(title)
+    plt.legend(loc=legend_loc)
+    plt.savefig("./graphs/static-analysis/" + file)
+    plt.cla()
+    return
+
+def plot_size_measures(repositories, val, title, file):
+    x = []
+    y = []
+    for i in range(0, len(repositories)):
+        x.append([val[i][3], val[i][2], val[i][1], val[i][0]])
+        y.append([repositories[i] + "_cyc", repositories[i] + "_ploc", repositories[i] + "_hal", repositories[i] + "_abc"])
+
+    font_size = 24
+    plt.rcParams.update({"font.size": font_size})
+    figure(figsize=(42, 22), dpi=80)
+
+    for i in range(0,len(repositories)):
+        bar = plt.barh(y[i], x[i], 0.66, label=repositories[i])
+        plt.bar_label(bar, padding=5, fontweight="bold")
+
+    plt.xticks(rotation=45)
+    plt.xlabel("Metric Values", loc="left",labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.ylabel("Repositories", loc="bottom", labelpad = 10, fontweight="bold", fontsize=font_size)
     plt.title(title)
     plt.legend()
     plt.savefig("./graphs/static-analysis/" + file)
     plt.cla()
-
-    #figure(figsize=(16, 10), dpi=80)
-    #plt.subplots_adjust(top=0.92, bottom=0.28)
-    #plt.rcParams.update({'font.size': 14})
-    #plt.xticks(rotation='vertical')
-    #plt.xlabel("Repositories")
-    #plt.ylabel(y_name)
-    #plt.title(title)
-    #for i in range(0, len(repositories)):
-    #    x = np.array([repositories[i] + "_sum", repositories[i] + "_min", repositories[i] + "_max", repositories[i] + "_avg"])
-    #    y = np.array([abc[i][3], abc[i][0], abc[i][1], abc[i][2]])
-    #    plt.bar(x, y, 0.5, label=repositories[i] + " " + versions[i])
-    #plt.legend()
-    #plt.savefig("./graphs/static-analysis/" + file)
     return
 
-
-def print_bar_plot(x,vec,img_path,title): 
-    V_axis = []
-    X_axis = np.arange(len(x))
-    Y_axis=[]
-    for v in x:
-        V_axis.append([v+'_AVG_CYC',v+'_MAX_CYC',v+'_MIN_CYC',v+'_AVG_COG',v+'_MAX_COG',v+'_MIN_COG'])
-    min_y=float("inf")
-    max_y=0
-    for rows in vec:
-        Y_axis.append([rows[0],rows[2],rows[4],rows[1],rows[3],rows[5]])
-    for y in Y_axis:
-        for value in y:
-            min_y=min(min_y,value)
-            max_y=max(max_y,value)
-    plt.rcParams.update({'font.size': 18})
-    figure(figsize=(36,30), dpi=80)
-    for i in range(0,len(x)):
-        bar=plt.barh(V_axis[i], Y_axis[i], 0.6, label = x[i])
-        plt.bar_label(bar,padding=5,fontweight='bold')
-
-    plt.xticks(get_ticks(Y_axis),rotation=45)
-    plt.xlim(min_y)
-    plt.xlabel("projects",loc='left',labelpad = 10,fontweight='bold',fontsize=22)
-    plt.ylabel("Complexity",loc='bottom',labelpad = 10,fontweight='bold',fontsize=22)
-    plt.title(title)
-    plt.legend()
-    plt.savefig(img_path)
-    plt.cla()
-    return
-
-
-def plot_size_measures(repositories, versions, val, title, y_name, file):
-    #figure(figsize=(16, 10), dpi=80)
-    #plt.subplots_adjust(top=0.92, bottom=0.28)
-    #plt.rcParams.update({'font.size': 14})
-    #plt.xticks(rotation='vertical')
-    #plt.xlabel("Repositories")
-    #plt.ylabel(y_name)
-    #plt.title(title)
-    #for i in range(0, len(repositories)):
-    #    x = np.array([repositories[i] + "_abc", repositories[i] + "_hal", repositories[i] + "_ploc", repositories[i] + "_cyc"])
-    #    y = np.array([val[i][0], val[i][1], val[i][2], val[i][3]])
-    #    plt.bar(x, y, 0.5, label=repositories[i] + " " + versions[i])
-    #plt.legend()
-    #plt.savefig("./graphs/static-analysis/" + file)
-
-    V_axis = []
-    Y_axis=[]
+def plot_complexity_measures(repositories, val, title, file):
+    x = []
+    y = []
     for i in range(0, len(repositories)):
-        V_axis.append([repositories[i] + "_cyc", repositories[i] + "_ploc", repositories[i] + "_hal", repositories[i] + "_abc"])
-        Y_axis.append([val[i][3], val[i][2], val[i][1], val[i][0]])
-    min_y=float("inf")
-    max_y=0
-    for y in Y_axis:
-        for value in y:
-            min_y=min(min_y,value)
-            max_y=max(max_y,value)
-    plt.rcParams.update({'font.size': 24})
-    figure(figsize=(42,22), dpi=80)
-    for i in range(0,len(repositories)):
-        bar=plt.barh(V_axis[i], Y_axis[i], 0.66, label = repositories[i])
-        plt.bar_label(bar,padding=5, fontweight='bold')
+        y.append([repositories[i] + "_cyc", repositories[i] + "_wmc"])
+        x.append([val[i][1], val[i][0]])
 
-    plt.xticks(get_ticks(Y_axis),rotation=45)
-    plt.xlim(min_y)
-    plt.xlabel("Repositories",loc='left',labelpad = 10, fontweight='bold', fontsize=24)
-    plt.ylabel(y_name,loc='bottom',labelpad = 10, fontweight='bold', fontsize=24)
+    font_size = 24
+    plt.rcParams.update({"font.size": font_size})
+    figure(figsize=(42, 22), dpi=80)
+
+    for i in range(0,len(repositories)):
+        bar = plt.barh(y[i], x[i], 0.66, label=repositories[i])
+        plt.bar_label(bar, padding=5, fontweight="bold")
+
+    plt.margins(y=0.1)
+    plt.xticks(rotation=45)
+    plt.xlabel("Metric Values", loc="left", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.ylabel("Repositories", loc="bottom", labelpad = 10, fontweight="bold", fontsize=font_size)
     plt.title(title)
     plt.legend()
     plt.savefig("./graphs/static-analysis/" + file)
     plt.cla()
-
-
     return
 
-def plot_complexity_measures(repositories, versions, val, title, y_name, file):
-    V_axis = []
-    Y_axis=[]
+def plot_visibility_measures(repositories, values, title, file):
+    y = []
+    x1 = []
+    x2 = []
     for i in range(0, len(repositories)):
-        V_axis.append([repositories[i] + "_cyc", repositories[i] + "_wmc"])
-        Y_axis.append([val[i][1], val[i][0]])
-    min_y=float("inf")
-    max_y=0
-    for y in Y_axis:
-        for value in y:
-            min_y=min(min_y,value)
-            max_y=max(max_y,value)
-    plt.rcParams.update({'font.size': 24})
-    figure(figsize=(42,20), dpi=80)
-    for i in range(0,len(repositories)):
-        bar=plt.barh(V_axis[i], Y_axis[i], 0.66, label = repositories[i])
-        plt.bar_label(bar,padding=5, fontweight='bold')
+        x1.append([values[i][1]])
+        x2.append([values[i][0]])
+        y.append([repositories[i]])
 
-    plt.xticks(get_ticks(Y_axis),rotation=45)
-    plt.xlim(min_y)
-    plt.xlabel("Repositories",loc='left',labelpad = 10, fontweight='bold', fontsize=24)
-    plt.ylabel(y_name,loc='bottom',labelpad = 10, fontweight='bold', fontsize=24)
+    font_size = 24
+    name = title.split()[-1]
+    plt.rcParams.update({"font.size": font_size})
+    figure(figsize=(42, 22), dpi=80)
+
+    for i in range(0, len(repositories)):
+        bar1 = plt.barh(y[i], x1[i], 0.4, color="#1f77b4", label = "Non-Public " + name)
+        bar2 = plt.barh(y[i], x2[i], 0.4, color="#d62728", label = "Public " + name)
+        plt.text((values[i][1] - values[i][0]) // 2 + values[i][0], i, str(int(values[i][1] - values[i][0])), color="snow", va="center", ha="center", fontweight="bold")
+        plt.bar_label(bar1, padding=5, fontweight="bold")
+        if values[i][0] != 0:
+            plt.bar_label(bar2, label_type="center", color="snow", fontweight="bold")
+
+    plt.margins(y=0.2)
+    plt.xticks(rotation=45)
+    plt.xlabel("Metric Values", loc="left", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.ylabel("Repositories", loc="bottom", labelpad = 10, fontweight="bold", fontsize=font_size)
     plt.title(title)
-    plt.legend()
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [1, 0]
+    plt.legend([handles[i] for i in order], [labels[i] for i in order], loc="upper right", prop={"size": font_size})
+    plt.savefig("./graphs/static-analysis/" + file)
+    plt.cla()
+    return
+
+def plot_visibility_percentages(repositories, values, title, file):
+    y = []
+    x1 = []
+    x2 = []
+    for i in range(0, len(repositories)):
+        x1.append([100])
+        x2.append([values[i][0] / values[i][1] * 100])
+        y.append([repositories[i]])
+
+    font_size = 24
+    name = title.split()[-1]
+    plt.rcParams.update({"font.size": font_size})
+    figure(figsize=(42, 22), dpi=80)
+
+    for i in range(0, len(repositories)):
+        bar1 = plt.barh(y[i], x1[i], 0.4, color="#1f77b4", label = "Non-Public " + name)
+        bar2 = plt.barh(y[i], x2[i], 0.4, color="#d62728", label = "Public " + name)
+        plt.text((100 - x2[i][0]) // 2 + x2[i][0], i, "{:.2f}".format(100 - x2[i][0]) + "%", color="snow", va="center", ha="center", fontweight="bold")
+        plt.bar_label(bar1, padding=5, fontweight="bold", fmt="%.0f%%")
+        if values[i][0] != 0:
+            plt.bar_label(bar2, label_type="center", color="snow", fontweight="bold", fmt="%.2f%%")
+
+    plt.margins(y=0.2)
+    plt.xticks(rotation=45)
+    plt.xlabel("Percentage Values", loc="left", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.ylabel("Repositories", loc="bottom", labelpad = 10, fontweight="bold", fontsize=font_size)
+    plt.title(title)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [1, 0]
+    plt.legend([handles[i] for i in order], [labels[i] for i in order], loc="upper right", prop={"size": font_size})
     plt.savefig("./graphs/static-analysis/" + file)
     plt.cla()
     return
@@ -197,31 +185,29 @@ def plot_complexity_measures(repositories, versions, val, title, y_name, file):
 def static_analysis():
     repos = ["FastCSV", "java-jwt", "jsoup", "Java-WebSocket"]
     versions = ["v2.2.0", "4.0.0", "jsoup-1.15.2", "v1.5.3"]
+
     abc = []
     npm = []
     npa = []
     wmc = []
-    coa = []
-    cda = []
     size = []
-    complexity = []
-    for i in range(len(repos)):
-        read_measures(repos[i], versions[i], abc, npm, npa, wmc, coa, cda, size, complexity)
-    plot_measures(repos, versions, abc, "ABC", "Magnitude", "abc-static-analysis.png")
-    plot_measures(repos, versions, npm, "NPM", "Number of public methods", "npm-static-analysis.png")
-    plot_measures(repos, versions, npa, "NPA", "Number of public attributes", "npa-static-analysis.png")
-    plot_measures(repos, versions, coa, "COA", "Class Operation Accessibility", "coa-static-analysis.png")
-    plot_measures(repos, versions, cda, "CDA", "Class Data Accessibility", "cda-static-analysis.png")
-    plot_measures(repos, versions, wmc, "WMC", "Weighted methods per class", "wmc-static-analysis.png")
-    plot_size_measures(repos, versions, size, "Size measures", "Values", "size-metrics-comparison.png")
-    plot_complexity_measures(repos, versions, complexity, "Complexity measures", "Values", "complexity-metrics-comparison.png")
-    return
+    compl = []
+    met = []
+    att = []
 
-def get_ticks(Y_axis):
-    vec=[y for ys in Y_axis for y in ys]
-    ymin= min(vec)
-    ymax= max(vec)
-    npv=np.linspace(ymin,ymax,30)
-    return npv
+    for i in range(len(repos)):
+        read_measures(repos[i], versions[i], abc, npm, npa, wmc, size, compl, met, att)
+
+    plot_cumulative_measures(repos, abc, "best", "ABC - Magnitude", "cumulative-measures-abc.png")
+    plot_cumulative_measures(repos, wmc, "best", "WMC - Weighted Methods per Class", "cumulative-measures-wmc.png")
+    plot_cumulative_measures(repos, npm, "best", "NPM - Number of Public Methods", "cumulative-measures-npm.png")
+    plot_cumulative_measures(repos, npa, "lower right", "NPA - Number of Public Attributes", "cumulative-measures-npa.png")
+    plot_visibility_measures(repos, met, "NPM - Number of Public Methods", "visibility-measures-npm.png")
+    plot_visibility_measures(repos, att, "NPA - Number of Public Attributes", "visibility-measures-npa.png")
+    plot_visibility_percentages(repos, met, "NPM - Number of Public Methods", "visibility-percentages-npm.png")
+    plot_visibility_percentages(repos, att, "NPA - Number of Public Attributes", "visibility-percentages-npa.png")
+    plot_complexity_measures(repos, compl, "Complexity measures", "metric-comparisons-complexity.png")
+    plot_size_measures(repos, size, "Size measures", "metric-comparisons-size.png")
+    return
 
 static_analysis()
